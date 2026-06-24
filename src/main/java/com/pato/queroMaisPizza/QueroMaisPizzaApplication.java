e43423a1a075e391ce24c304003db5b82c45801e
Mateus Souza <mateussouzaprogrammer@gmail.com>
2026-06-24 16:28:35 -0400

    Start project

---
+++ src/main/java/com/pato/queroMaisPizza/QueroMaisPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.pato.queroMaisPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueroMaisPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueroMaisPizzaApplication.class, args);
+	}
+
+}
